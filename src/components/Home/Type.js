e79d5995785d77b0f082a0e4d1e8eab4a7c22006
Mason Hames <m.hames2@yahoo.com>
2022-01-26 20:31:24 -0500

    initial add

---
+++ src/components/Home/Type.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Web Developer",
+          "MERN Stack Developer",
+          "Coffee Lover"
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
